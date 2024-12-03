f75f489f374449982fcd280f287737c188306c85
Quintinno Developer <quintinno.developer@gmail.com>
2024-12-03 08:47:34 -0300

    🔵 feature: Implementar endpoint da funcionalidade de gerenciador de pessoas

--- src/main/java/br/com/quintinno/ketertorahapi/controller/PaisController.java
+++ src/main/java/br/com/quintinno/ketertorahapi/controller/PaisController.java
@@ -3,6 +3,7 @@ package br.com.quintinno.ketertorahapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import br.com.quintinno.ketertorahapi.service.PaisService;
 
 @RestController
 @RequestMapping("/api/pais")
+@CrossOrigin("*")
 public class PaisController {
 
     @Autowired
