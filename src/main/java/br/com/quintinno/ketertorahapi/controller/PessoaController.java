f59a418e5f9978b6fc777d238c8c5f0b3d61c16f
Quintinno Developer <quintinno.developer@gmail.com>
2024-12-02 21:38:15 -0300

    readme.md

--- src/main/java/br/com/quintinno/ketertorahapi/controller/PessoaController.java
+++ src/main/java/br/com/quintinno/ketertorahapi/controller/PessoaController.java
@@ -1,6 +1,9 @@
 package br.com.quintinno.ketertorahapi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,11 @@ public class PessoaController {
     @PostMapping("/v1")
     public PessoaEntity create(@RequestBody PessoaEntity pessoaEntity) {
         return this.pessoaService.create(pessoaEntity); 
+    }
+
+    @GetMapping("/v1")
+    public List<PessoaEntity> readAll() {
+        return this.pessoaService.readAll();
     }
 
 }
--- src/main/java/br/com/quintinno/ketertorahapi/service/PessoaService.java
+++ src/main/java/br/com/quintinno/ketertorahapi/service/PessoaService.java
@@ -1,5 +1,7 @@
 package br.com.quintinno.ketertorahapi.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,10 @@ public class PessoaService {
 
     public PessoaEntity create(PessoaEntity pessoaEntity) {
         return this.pessoaRepository.save(pessoaEntity);
+    }
+
+    public List<PessoaEntity> readAll() {
+        return this.pessoaRepository.findAll();
     }
 
 }
